fd88735f18f6620e86844090f51c63eba49686e4
Dino <dino.talundzic.sts@gmail.com>
2019-01-08 00:18:48 +0100

    Added table of ride offers on welcome page

--- login.php
+++ login.php
@@ -1,7 +1,9 @@
 <?php
 	require "init.php";
+	session_start();
 	
 	$uporabnisko_ime=$_POST["login_ime"];
+	$_SESSION['sessionName']=$uporabnisko_ime;
 	$geslo=$_POST["login_geslo"];
 	
 	//check if there is a row with the same username and check if the password matches with the hash
